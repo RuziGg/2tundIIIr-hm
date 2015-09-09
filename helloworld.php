e844d3008bbb90416f2ef9c9fd6e7688c6991281
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2015-09-09 12:25:19 +0300

    Ruslan added

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,45 @@
-<?php
+﻿<?php
 	$first_name = "Ruslan";
 	$last_name = "Jegorov";
 	
 	echo $first_name." ".$last_name;
+?>
+<br>
+<?php
+
+	$age = 19;
+	
+	//(Komentaarid) tetib loogikat, juhul kui vanus on väiksem
+	//kui 18 siis kirjuta "alaealine", muul juhul "täisealine"
+	
+	// ifi sisse loogikatehe
+	if($age < 18) {
+		//tõene
+	echo "alaealine";
+	
+} else {
+	//väär
+	echo "täisealine";
+}
+
+?>
+<br>
+<?php
+	//vastavalt vanusele trükime niimitu korda välja sõna "palju"
+	
+	for($i = 0; $i < $age; $i = $i + 1) {
+		
+		//tegevus mis kordub
+		echo "palju ".$i." ";
+	}
+	
+	echo "õnne!";
+
+?>
+<br>
+<?php
+	// Trüki välja kuupäev kujul: nädalapäev, kp kuu aasta
+	echo date("l, j F Y e");
+
+
 ?>
